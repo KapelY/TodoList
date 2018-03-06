02eb89eed6c5d126c5d2b0411f3dd90beaf41eb8
Yuriy Kapelyshny <zig.tos@gmail.com>
2018-03-06 14:09:35 +0200

    Initial commit

---
+++ src/com/zig/todolist/Main.java
@@ -0,0 +1,43 @@
+package com.zig.todolist;
+
+import com.zig.todolist.datamodel.TodoData;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class Main extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("mainwindow.fxml"));
+        primaryStage.setTitle("Todo List");
+        primaryStage.setScene(new Scene(root, 900, 500));
+        primaryStage.show();
+    }
+
+    @Override
+    public void stop() {
+        try {
+            TodoData.getInstance().storeToDoItems();
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    @Override
+    public void init() throws Exception {
+        try {
+            TodoData.getInstance().loadTodoItems();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
---
+++ src/com/zig/todolist/test.java
@@ -0,0 +1,9 @@
+package com.zig.todolist;
+
+import java.util.Date;
+
+public class test {
+    public static void main(String[] args) {
+        long date = new Date().getTime();
+    }
+}
